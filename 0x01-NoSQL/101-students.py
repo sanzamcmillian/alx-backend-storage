026edfaed01d106f10089b86fed985dce13896e6
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-16 12:57:04 +0200

    task 101

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""returns all students sorted by average score"""
+
+
+def top_students(mongo_collection):
+    """function that returns all students sorted by average score"""
+    students = mongo_collection.aggregate(
+        [
+            {
+                "$project": {
+                    "_id": 1,
+                    "name": 1,
+                    "averageScore": {
+                        "$avg": {
+                            "$avg": "$topics.score",
+                        },
+                    },
+                    "topics": 1,
+                },
+            },
+            {
+                "$sort": {"averageScore": -1},
+            }
+        ]
+    )
+    return students
