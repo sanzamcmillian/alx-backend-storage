e8ef73ebf23c3109b2bc81324c1927fb257e88b9
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-17 16:30:04 +0200

    task 1

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""_summary_"""
+from typing import Union
+import uuid
+import redis
+
+
+class Cache:
+    def __init__(self) -> None:
+        self._redis = redis.Redis()
+        self._redis.flushdb(True)        
+    
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ a method to generate a random key"""
+        key_generate = str(uuid.uuid4())
+        self._redis.set(key_generate, data)
+        return key_generate
