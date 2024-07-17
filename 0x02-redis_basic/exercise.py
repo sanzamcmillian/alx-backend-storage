ec39af53502bce73d4d8f28865c3f9f3df6ea007
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-17 16:56:55 +0200

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """_summary_"""
-from typing import Union,Callable
+from typing import Union, Callable
 import uuid
 import redis
 
@@ -15,16 +15,16 @@ class Cache:
         key_generate = str(uuid.uuid4())
         self._redis.set(key_generate, data)
         return key_generate
-    
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes]:
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, int, float, bytes]:
         """ a method to convert the data back to the desired redis format """
         catched = self._redis.get(key)
         return fn(catched) if fn is not None else catched
-    
+
     def get_str(self, key: str) -> str:
-        """a method that automatically parametrize Cache.get with the correct conversion function."""
+        """a method that parametrize Cache.get with a string conversion ."""
         return self.get(key, lambda X: x.decode("utf-8"))
-    
+
     def get_int(self, key: str) -> int:
-        """a method that automatically parametrize Cache.get with the correct conversion function."""
+        """a method that parametrize Cache.get with the integer conversion."""
         return self.get(key, lambda x: int(x))
