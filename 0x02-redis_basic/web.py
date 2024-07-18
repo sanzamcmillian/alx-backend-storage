fd33a44bf048b2d71dabc0d65e417600f5f73a17
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-18 13:31:32 +0200

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -20,7 +20,7 @@ def cacher(method: Callable) -> Callable:
         if result:
             return result.decode("utf-8")
         result = method(url)
-        redis_store.set(f"count:{url}")
+        redis_store.set(f"count:{url}", 0)
         redis_store.setex(f"result:{url}", 10, result)
         return result
     return invoke
