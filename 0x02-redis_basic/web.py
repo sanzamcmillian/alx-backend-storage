c7d299fedfc7eb8482f63c963c0674b7da350314
sanzamcmillian <g18s2658@ru.ac.za>
2024-07-18 13:27:55 +0200

    Create web.py

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env pyhton3
+"""Implementing an expiring web cache and tracker"""
+import requests
+import redis
+from functools import wraps
+from typing import Callable
+
+
+redis_store = redis.Redis()
+
+
+def cacher(method: Callable) -> Callable:
+    """caches the data """
+    @wraps(method)
+    def invoke(url) -> str:
+        """wrapper method"""
+        redis_store.incr(f"count:{url}")
+        result = redis_store.get(f"result:{url}")
+        if result:
+            return result.decode("utf-8")
+        result = method(url)
+        redis_store.set(f"count:{url}")
+        redis_store.setex(f"result:{url}", 10, result)
+        return result
+    return invoke
+
+
+@cacher
+def get_page(url: str) -> str:
+    """requests module to obtain the HTML content of a URL"""
+    return requests.get(url).text
